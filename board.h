af2fc4bae8ef240f69ce834e911a333aa861f7b0
wengjae <77240633+Jaee-C@users.noreply.github.com>
2021-10-01 16:44:27 +0800

    initialise board, and print initialised board

---
+++ board.h
@@ -0,0 +1,24 @@
+/* some #define's from my sample solution ------------------------------------*/
+#define BOARD_SIZE          8       // board size
+#define ROWS_WITH_PIECES    3       // number of initial rows with pieces
+#define CELL_EMPTY          '.'     // empty cell character
+#define CELL_BPIECE         'b'     // black piece character
+#define CELL_WPIECE         'w'     // white piece character
+#define CELL_BTOWER         'B'     // black tower character
+#define CELL_WTOWER         'W'     // white tower character
+#define COST_PIECE          1       // one piece cost
+#define COST_TOWER          3       // one tower cost
+#define TREE_DEPTH          3       // minimax tree depth
+#define COMP_ACTIONS        10      // number of computed actions
+#define ROW_SEP             "+---+---+---+---+---+---+---+---+"
+#define DELIMITER_LEN       37
+
+/* one type definition from my sample solution -------------------------------*/
+typedef unsigned char board_t[BOARD_SIZE][BOARD_SIZE];  // board type
+
+void print_head(void);
+void board_init(board_t);
+int cell_has_piece(int row, int col);
+void print_board(board_t);
+void print_row_sep(void);
+void print_delimiter(void);
---
+++ board_init.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "board.h"
+
+void print_head(void) {
+    printf("BOARD SIZE: %dx%x\n", BOARD_SIZE, BOARD_SIZE);
+    printf("#BLACK PIECES: %d\n", BOARD_SIZE * ROWS_WITH_PIECES / 2);
+    printf("#WHITE PIECES: %d\n", BOARD_SIZE * ROWS_WITH_PIECES / 2);
+}
+
+void board_init(board_t board) {
+    int row_has_piece = 0;
+    char piece;
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        if (row < ROWS_WITH_PIECES) {
+            row_has_piece = 1;
+            piece = CELL_WPIECE;
+        } else if (BOARD_SIZE - ROWS_WITH_PIECES <= row) {
+            row_has_piece = 1;
+            piece = CELL_BPIECE;
+        } else {
+            row_has_piece = 0;
+        }
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            if (row_has_piece && cell_has_piece(row, col)) {
+                board[row][col] = piece;
+            } else {
+                board[row][col] = CELL_EMPTY;
+            }
+        }
+    }
+}
+
+int cell_has_piece(int row, int col) {
+    if ((row % 2 == 0 && col % 2 != 0) || 
+        (row % 2 != 0 && col % 2 == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void print_board(board_t board) {
+    char col_name = 'A';
+    printf("  ");
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        printf("   %c", col_name);
+        col_name++;
+    }
+    printf("\n");
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        print_row_sep();
+        printf("%2d | ", row + 1);
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            printf("%c | ", board[row][col]);
+        }
+        printf("\n");
+    }
+    print_row_sep();
+    print_delimiter();
+}
+
+void print_row_sep(void) {
+    printf("   %s\n", ROW_SEP);
+}
+
+void print_delimiter(void) {
+    for (int i = 0; i < DELIMITER_LEN; i++) {
+        printf("%c",'=');
+    }
+    printf("\n");
+}
